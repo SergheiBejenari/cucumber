36dca373fad95507d6199faf17dc80a0ce591313
Serghei <My.cup.of.coffee.with.milk@gmail.com>
2021-04-14 16:41:39 +0300

    Add TransferFunds.feature

---
+++ src/test/java/steps/TransferFundsStep.java
@@ -0,0 +1,44 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TransferFundsStep {
+
+    @Given("Following users have positive bill")
+    public void followingUsersHavePositiveBill() {
+
+    }
+
+    @When("User is willing to perform Person To Person transfer amount to another User")
+    public void userIsWillingToPerformPersonToPersonTransferAmountToAnotherUser() {
+
+    }
+
+    @And("Success message is displayed")
+    public void successMessageIsDisplayed() {
+
+    }
+
+    @And("The funds were debited in the amount in which they were sent")
+    public void theFundsWereDebitedInTheAmountInWhichTheyWereSent() {
+
+    }
+
+    @Then("The user's account has increased by the amount of money that was sent")
+    public void anotherUserDataCorrespondsToFollowingDetails() {
+
+    }
+
+    @And("The outgoing Person To Person transfer operation appears on the client's statement")
+    public void theOutgoingPersonToPersonTransferOperationAppearsOnTheClientSStatement() {
+
+    }
+
+    @And("The incoming Person To Person transfer operation appears on another client's statement")
+    public void theIncomingPersonToPersonTransferOperationAppearsOnAnotherClientSStatement() {
+
+    }
+}
---
+++ src/test/resources/features/TransferFunds.feature
@@ -0,0 +1,10 @@
+Feature: Transfer funds between different users
+
+  Scenario: Transfer funds between different users
+    Given Following users have positive bill
+    When User is willing to perform Person To Person transfer amount to another User
+    And Success message is displayed
+    And The funds were debited in the amount in which they were sent
+    Then The user's account has increased by the amount of money that was sent
+    And The outgoing Person To Person transfer operation appears on the client's statement
+    And The incoming Person To Person transfer operation appears on another client's statement
